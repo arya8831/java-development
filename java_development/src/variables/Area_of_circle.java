48c95cad8d661d255bfebe93eb3060944aae7488
chatt <arya_c.cse2021@msit.edu.in>
2023-08-26 20:05:57 +0530

    commit2

---
+++ java_development/src/variables/Area_of_circle.java
@@ -0,0 +1,18 @@
+package variables;
+import java.util.*;
+
+public class Area_of_circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter radius");
+		float radius=sc.nextFloat();
+		
+		float area=3.14f * radius*radius;
+		System.out.println("area is"+area);
+		
+
+	}
+
+}
---
+++ java_development/src/variables/Type_casting.java
@@ -0,0 +1,13 @@
+package variables;
+	import java.util.*;
+public class Type_casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter a no :");
+		int b=(int)sc.nextFloat();
+		System.out.println(b);
+	}
+
+}
