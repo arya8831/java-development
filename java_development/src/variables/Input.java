50ae369b9b53af8c84f26a55923e77596b61ea6d
chatt <arya_c.cse2021@msit.edu.in>
2023-08-26 18:08:31 +0530

    commit1

---
+++ java_development/src/variables/Input.java
@@ -0,0 +1,23 @@
+package variables;
+ import java.util.*;
+
+public class Input {
+
+	public Input() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		Scanner sc = new Scanner(System.in);
+		String name =sc.nextLine();
+		System.out.println(name);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
