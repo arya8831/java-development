bc6f046824f434759cd4ee45f57dd7ab1bf05702
chatt <arya_c.cse2021@msit.edu.in>
2023-10-01 00:38:26 +0530

    ca3

---
+++ java_development/src/ca3/Method_overloading.java
@@ -0,0 +1,21 @@
+package ca3;
+
+public class Method_overloading {
+	String name;
+	int age;
+	
+
+	
+	public void info(String name) {
+		System.out.println("name is "+this.name);
+
+	}
+	public void info(int age) {
+
+		System.out.println("age is "+this.age);
+	}
+	public void info(String name , int age) {
+		System.out.println("name is "+this.name);
+		System.out.println("age is "+this.age);
+	}
+}
---
+++ java_development/src/ca3/Method_overloading_impl.java
@@ -0,0 +1,24 @@
+package ca3;
+
+import java.util.Scanner;
+
+public class Method_overloading_impl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter name :");
+		 String name=sc.next();
+		System.out.print("enter age :");
+		 int age=sc.nextInt();
+		 Method_overloading s1= new Method_overloading();
+			s1.name=name;
+			s1.age=age;
+			//s1.info(name , age);
+			System.out.println("age is "+s1.age);
+			
+	}
+	
+	
+
+}
